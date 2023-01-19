df1ed498d1d7f0510ae953d35b2953c8342eb47d
DaltonPaul <108111570+DaltonPaul@users.noreply.github.com>
2023-01-18 18:21:58 -0800

    Create Introduction to Programming.py

---
+++ Introduction to Programming.py
@@ -0,0 +1,26 @@
+print("Hello World")
+
+print\
+    ("This is a really long line of code on a different line")
+
+#10 times
+for i in range(10):
+    print("Hello World")
+
+home = "America"
+if home == "America" :
+    print("Hello, America")
+
+home = "World"
+if home == "America" :
+    print("Hello America")
+else:
+    print("Hello World")
+
+home = "World"
+if home == "America":
+    print("Hello America")
+elif home == "Europe":
+    print("Hello Europe")
+else:
+    print("Hello World")
