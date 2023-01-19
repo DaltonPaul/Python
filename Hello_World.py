f93d1a8bd7ca8a58cc45fc611e16e99b1494d74e
DaltonPaul <108111570+DaltonPaul@users.noreply.github.com>
2023-01-18 17:43:12 -0800

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,4 +1,5 @@
-Python 3.11.1 (v3.11.1:a7a450f84a, Dec  6 2022, 15:24:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print("Hello World")
-Hello World
+print("Hello World")
+
+#100 times
+for i in range(100):
+    print("Hello World")
